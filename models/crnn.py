51dc8ec347a91a022acd523c9f312777455faca4
Alexander Gunin <guninalexander@gmail.com>
2019-07-01 15:32:31 +0300

    numbers

--- models/crnn.py
+++ models/crnn.py
@@ -307,7 +307,7 @@ def _crnn_model_fn(features, labels, mode, params=None, config=None):
                                  kernel_initializer=tf.contrib.layers.xavier_initializer())
 
     if params['beam_search_decoder']:
-        decoded, _log_prob = tf.nn.ctc_beam_search_decoder(logits, input_lengths)
+        decoded, _log_prob = tf.nn.ctc_beam_search_decoder(logits, input_lengths,merge_repeated=False)
     else:
         decoded, _log_prob = tf.nn.ctc_greedy_decoder(logits, input_lengths)
 
