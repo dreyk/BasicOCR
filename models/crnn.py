3ed0397bea7bb1b22d5b909ed533a6ecfe4a5b20
Alexander Gunin <guninalexander@gmail.com>
2019-07-01 12:59:26 +0300

    try numbers

--- models/crnn.py
+++ models/crnn.py
@@ -17,6 +17,8 @@ ENGLISH_CHAR_MAP = [
     '#',
     # Alphabet normal
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    ','
+    ' '
     # "end of sentence" character for CTC algorithm
     '_'
 ]
