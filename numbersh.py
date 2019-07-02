5b051451187c75115d7ebbdf00452ac164da7837
Alexander Gunin <guninalexander@gmail.com>
2019-07-02 08:55:21 +0300

    numbers

--- numbersh.py
+++ numbersh.py
@@ -7,7 +7,15 @@ LOG.setLevel(logging.INFO)
 
 ENGLISH_CHAR_MAP = [
     '#',
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    # Alphabet normal
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '0','1','2','3','4','5','6','7','8','9',
+    '-',':','(',')','.',',','/'
+    # Apostrophe only for specific cases (eg. : O'clock)
+                            "'",
+    " ",
+    # "end of sentence" character for CTC algorithm
     '_'
 ]
 
