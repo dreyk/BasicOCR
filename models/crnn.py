9dc46d1236360951a95bb01ac9c581158fd8fd39
Alexander Gunin <guninalexander@gmail.com>
2019-07-04 12:14:03 +0300

    add fonts

--- models/crnn.py
+++ models/crnn.py
@@ -336,6 +336,7 @@ def _crnn_model_fn(features, labels, mode, params=None, config=None):
     logits = logits*weights
 
     if params['beam_search_decoder']:
+        logits = logits*weights
         decoded, _log_prob = tf.nn.ctc_beam_search_decoder(logits, input_lengths, merge_repeated=False)
     else:
         decoded, _log_prob = tf.nn.ctc_greedy_decoder(logits, input_lengths)
